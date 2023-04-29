9b389c2b832fc2398c2f1bac91312d5f29149fbc
Rhonnie Al <rhonnieallan@protonmail.com>
2023-04-29 18:42:05 +0300

    Init

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3300;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Welocome to the Homepage</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
