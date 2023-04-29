9cd3ad37e8bc2e23257913715c66f7bcc1ce43dd
Rhonnie Al <rhonnieallan@protonmail.com>
2023-04-29 20:28:10 +0300

    Add "chat" endpoint

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ const port = process.env.PORT || 3300;
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Welcome to the Homepage</h1>");
+});
+
+app.post("/chat", (req, res) => {
+  console.log("You have been Pinged!");
+  console.log(req.body);
+
+  if (req.body.userprompt) {
+    res.status(200).send("received");
+  } else {
+    res.status(400).send("Error");
+  }
 });
 
 app.listen(port, () => {
